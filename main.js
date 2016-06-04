c0d10372febc8e6854af79df9e2a7f3acf6bbd31
steinwedel <github.com@steinwedel.de>
2016-06-04 16:22:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
- 
+"use strict";
+
+var utils = require(__dirname + '/lib/utils');
+var adapter = utils.adapter('smartercoffee');
+
+adapter.on('ready', function () {
+    if (adapter.config.ip) {
+
+        var auth = {
+            "ip": adapter.config.ip
+        };
+
+        //api = new netatmo(auth);
+        //Hier connect
+
+        // Update
+        requestUpdate();
+    } else
+        adapter.log.error("Please setup IP of your smarter coffee machine!");
+});
+
